c857ca29bd68139cc71044ecda9beaec28ce0b5e
Peter <33762147+pdani013@users.noreply.github.com>
2022-08-02 07:42:05 -0600

    Update MetaMaskCard.tsx

--- components/connectorCards/MetaMaskCard.tsx
+++ components/connectorCards/MetaMaskCard.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { hooks, metaMask } from "../../connectors/metaMask";
 import { ConnectWithSelect } from "../ConnectWithSelect";
-import { Status } from "../Status";
+// import { Status } from "../Status";
 import React from "react";
 
 const {
